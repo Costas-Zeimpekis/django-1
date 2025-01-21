20aabfaff89a652b38d1af5392449f0d64a6ebc2
Costas Zeimpekis <morpheustudio@gmail.com>
2025-01-21 15:35:03 +0200

    first commit

---
+++ my_tennis_club/members/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+
+def members(request):
+    template = loader.get_template('all_members.html')
+    members = Member.objects.all().values()
+    context = {
+        'members': members
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    member = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'member': member,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+
+def testing(request):
+    template = loader.get_template('testing.html')
+    fruits = ['Apple', 'Orange', 'Lemon']
+    context = {
+        'fruits': fruits
+    }
+    return HttpResponse(template.render(context, request))
